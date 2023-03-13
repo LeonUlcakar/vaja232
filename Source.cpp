296eabff8f5e8cf4ab1bbfc3809ccccd282e6745
LeonUlcakar <leon.ulcakar@gmail.com>
2023-03-13 11:02:39 +0100

    why no wrok

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <complex.h>
-#define SIZE 1000
+#include "bitmap.h"
 using namespace std;
+#define SIZE 1000
 
 double square(int num, double resolution) {
     double root = 1;
@@ -32,18 +33,17 @@ int main() {
     double complex r2 = -0.5 + sqrt(3) / 2 * I;
     double complex r3 = -0.5 - sqrt(3) / 2 * I;
     double picture[SIZE][SIZE];
+
     int temp = 0;
-    double complex z;
+    double complex z = 0;
     for (int i = -SIZE / 2; i < SIZE / 2; i++) {
         for (int j = -SIZE / 2; j < SIZE / 2; j++) {
             z = cubicroot(i + j * I);
-            //printf("Z = %.2f %+.2fi\n", creal(z), cimag(z));
             if (z == r1) {
                 picture[i + SIZE / 2][j + SIZE / 2] = 80;
             }
             else if (z == r2) {
                 picture[i + SIZE / 2][j + SIZE / 2] = 160;
-
             }
             else if (z == r3) {
                 picture[i + SIZE / 2][j + SIZE / 2] = 240;
